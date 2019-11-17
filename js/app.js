579697358009195df648ea271bc2bf707fd8bf63
Edward Williams <edward.williams91@gmail.com>
2019-11-17 17:46:51 -0600

    Basic backend (admin view) is up, along with deleting entries. Not pretty!

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 
 //change screens for testing
 //change 0 for production release
-var screen = 0
+var screen = 0;
 //0-Firstname
 //1-Lastname
 //2-Sid
@@ -23,135 +23,143 @@ var student = {
 	grade: null,
 	problem: [],
 	rfid: '',
-	notes: ''
-}
+	notes: 'N/A',
+};
 
 var writeData = () => {
-	var d = new Date()
-	var e = d.valueOf()
-	firebase.database().ref("students/" + e).set({
-		timestamp: d.toDateString() + " " + d.toLocaleTimeString(),
-		firstName: student.firstName,
-		lastName: student.lastName,
-		sid: student.sid,
-		grade: student.grade,
-		problem: student.problem,
-		notes: ''
-	});
-	setTimeout(() => { location.reload() }, 5000)
-}
-const submitButton = document.querySelector("#submit-button");
+	var d = new Date();
+	var e = d.valueOf();
+	firebase
+		.database()
+		.ref('students/' + e)
+		.set({
+			timestamp: d.toDateString() + ' ' + d.toLocaleTimeString(),
+			firstName: student.firstName,
+			lastName: student.lastName,
+			sid: student.sid,
+			grade: student.grade,
+			problem: student.problem,
+			notes: '',
+		});
+	setTimeout(() => {
+		location.reload();
+	}, 5000);
+};
+const submitButton = document.querySelector('#submit-button');
 
 //Updates Student Object
 //document.addEventListener("keyup", e => { //every key up
-var updates = document.body.querySelectorAll(".update")
+var updates = document.body.querySelectorAll('.update');
 
 for (const button of updates) {
-	button.addEventListener("click", (e) => {
-		student.firstName = document.getElementById("first-name").value
-		student.lastName = document.getElementById("last-name").value
-		student.sid = document.getElementById("sid").value
+	button.addEventListener('click', e => {
+		student.firstName = document.getElementById('first-name').value;
+		student.lastName = document.getElementById('last-name').value;
+		student.sid = document.getElementById('sid').value;
 		//student.rfid = document.getElementById("rfid").value  //RF ID value removed
-		confirmData()
+		confirmData();
 		//console.log(student)
-	})
+	});
 }
 
 //Grade Selector - Sets student.grade
-const gradeButtons = document.getElementById("grade-screen").querySelectorAll('button')
+const gradeButtons = document
+	.getElementById('grade-screen')
+	.querySelectorAll('button');
 for (const button of gradeButtons) {
-	button.addEventListener("click", e => {
-		student.grade = parseInt(button.value)
+	button.addEventListener('click', e => {
+		student.grade = parseInt(button.value);
 		for (const button2 of gradeButtons) {
-			button2.classList.remove("active")
+			button2.classList.remove('active');
 		}
-		e.target.classList.add("active")
-	})
+		e.target.classList.add('active');
+	});
 }
 
 //Issue Selector - Adds/Remove Problems to student.problem
-const problemButtons = document.getElementById("problem-screen").querySelectorAll('button')
+const problemButtons = document
+	.getElementById('problem-screen')
+	.querySelectorAll('button');
 for (const button of problemButtons) {
-	button.addEventListener("click", e => {
+	button.addEventListener('click', e => {
 		if (student.problem.includes(button.textContent) == false) {
-			student.problem.push(button.textContent)
-			e.target.classList.add("active")
+			student.problem.push(button.textContent);
+			e.target.classList.add('active');
 		} else {
-			student.problem.splice(student.problem.indexOf(button.textContent), 1)
-			e.target.classList.remove("active")
+			student.problem.splice(student.problem.indexOf(button.textContent), 1);
+			e.target.classList.remove('active');
 		}
 		//console.log(student)
-	})
+	});
 }
 
 //Confirmation Data
 const confirmData = () => {
-
-	var confirmScreen = document.getElementById('confirm')
-	confirmScreen.querySelector('.confirm-name').textContent = student.firstName + " " + student.lastName
-	confirmScreen.querySelector('.confirm-id').textContent = student.sid
-	confirmScreen.querySelector('.confirm-grade').textContent = student.grade
-	confirmScreen.querySelector('.confirm-issue').innerHTML = ''
+	var confirmScreen = document.getElementById('confirm');
+	confirmScreen.querySelector('.confirm-name').textContent =
+		student.firstName + ' ' + student.lastName;
+	confirmScreen.querySelector('.confirm-id').textContent = student.sid;
+	confirmScreen.querySelector('.confirm-grade').textContent = student.grade;
+	confirmScreen.querySelector('.confirm-issue').innerHTML = '';
 	for (x = 0; x < student.problem.length; x++) {
-		var li = document.createElement('LI')
-		var text = student.problem[x]
-		li.textContent = text
-		confirmScreen.querySelector('.confirm-issue').appendChild(li)
+		var li = document.createElement('LI');
+		var text = student.problem[x];
+		li.textContent = text;
+		confirmScreen.querySelector('.confirm-issue').appendChild(li);
 	}
-}
+};
 
 //Screen Selector Logic
-const screenControl = (con) => {
+const screenControl = con => {
 	// console.log(screen)
 	if (con == 1) {
 		if (screen == 0 && student.firstName != '') {
-			screen++
-		}
-		else if (screen == 1 && student.lastName != '') {
-			screen++
-		}
-		else if (screen == 2 && student.sid != '') {
-			screen++
-		}
-		else if (screen == 3 && student.grade != null) {
-			screen++
-		}
-		else if (screen == 4 && student.problem != []) {
-			screen++
-		}
-		else if (screen == 5) {
-			screen++
+			screen++;
+		} else if (screen == 1 && student.lastName != '') {
+			screen++;
+		} else if (screen == 2 && student.sid != '') {
+			screen++;
+		} else if (screen == 3 && student.grade != null) {
+			screen++;
+		} else if (screen == 4 && student.problem != []) {
+			screen++;
+		} else if (screen == 5) {
+			screen++;
 		}
 	} else {
-		screen--
+		screen--;
 	}
 
-	screenCheck(screen)
-}
+	screenCheck(screen);
+};
 //screenCheck(screen)
-const screenCheck = (on) => {
-	var screens = document.getElementsByClassName('screen')
+const screenCheck = on => {
+	var screens = document.getElementsByClassName('screen');
 	//console.log(screens)
 	for (x = 0; x < screens.length; x++) {
 		//console.log(x)
 		if (x == on) {
-			screens[x].classList.remove('hide')
+			screens[x].classList.remove('hide');
 		} else {
-			screens[x].classList.add('hide')
+			screens[x].classList.add('hide');
 		}
 	}
-}
+};
 
 //Next/Back button logic
-const nextButtons = document.getElementsByClassName('next')
+const nextButtons = document.getElementsByClassName('next');
 for (const button of nextButtons) {
-	button.addEventListener('click', e => { screenControl(1) })
+	button.addEventListener('click', e => {
+		screenControl(1);
+	});
 }
-const backButtons = document.getElementsByClassName('back')
+const backButtons = document.getElementsByClassName('back');
 for (const button of backButtons) {
-	button.addEventListener('click', e => { screenControl(-1) })
+	button.addEventListener('click', e => {
+		screenControl(-1);
+	});
 }
-submitButton.addEventListener("click", writeData);
+submitButton.addEventListener('click', writeData);
 
 // //Submit Reset Timer
 // const complete = () => {
---
+++ js/backend.js
@@ -0,0 +1,170 @@
+// var writeData = () => {
+// 	var d = new Date()
+// 	var e = d.valueOf()
+// 	firebase.database().ref("students/" + e).set({
+// 		timestamp: d.toDateString() + " " + d.toLocaleTimeString(),
+// 		firstName: student.firstName,
+// 		lastName: student.lastName,
+// 		sid: student.sid,
+// 		grade: student.grade,
+// 		problem: student.problem,
+// 		notes: ''
+// 	});
+// 	setTimeout(() => { location.reload() }, 5000)
+// }
+
+const signInList = document.getElementById('signin-list');
+
+//listen for changes and refresh
+// var startListening = function() {
+// 	firebase.database().on('value', function(snapshot) {
+// 		var warehouse = snapshot.child('prod_id/stats').val();
+// 		document.getElementById('qty').innerHTML = warehouse.qty;
+// 	});
+// };
+// startListening();
+
+var removeData = signinListing => {
+	var e = signinListing;
+	firebase
+		.database()
+		.ref('students/' + e)
+		.remove();
+	location.reload();
+};
+
+var getData = () => {
+	firebase
+		.database()
+		.ref('students/')
+		.once('value', function(snapshot) {
+			snapshot.forEach(function(signinSnapshot) {
+				//C
+				//var signinKey = signinSnapshot.key;
+				var signinData = signinSnapshot.val();
+				//create basic divs... not very dry...
+				//can probably loop though an object to make this dry.
+				var listItem = document.createElement('DIV');
+				listItem.classList += 'list-item';
+				//divs
+
+				var uidDiv = document.createElement('DIV'); //hidden div containing UID info from database
+				uidDiv.classList += 'hide';
+				uidDiv.innerText = signinSnapshot.key;
+				//console.log(signinSnapshot.key);
+
+				var nameDiv = document.createElement('DIV');
+				nameDiv.classList += 'name';
+				nameDiv.innerText =
+					signinData['lastName'] + ', ' + signinData['firstName'];
+
+				var gradeDiv = document.createElement('DIV');
+				gradeDiv.classList += 'grade';
+				gradeDiv.innerText = signinData['grade'];
+
+				var sidDiv = document.createElement('DIV');
+				sidDiv.classList += 'sid';
+				sidDiv.innerText = signinData['sid'];
+				var copyIcon = document.createElement('I');
+
+				copyIcon.classList += 'fi-xnluxl-copy';
+
+				var problemDiv = document.createElement('DIV');
+				//problemDiv.innerText = signinData['problem'];
+				problemDiv.classList += 'problem';
+				//console.log(signinData['problem'].length);
+
+				//Below For loop stolen from index for reuse
+				for (x = 0; x < signinData['problem'].length; x++) {
+					//console.log(signinData['problem'][x]);
+					var p = document.createElement('p');
+					var text = signinData['problem'][x];
+					p.textContent = text;
+					//confirmScreen.querySelector('.confirm-issue').appendChild(li)
+					problemDiv.appendChild(p);
+				}
+
+				var notesDiv = document.createElement('DIV');
+				notesDiv.classList += 'notes';
+				notesDiv.innerText = signinData['notes'];
+
+				var timeDiv = document.createElement('DIV');
+				timeDiv.classList += 'time';
+				timeDiv.innerText = signinData['timestamp'];
+
+				var controlsDiv = document.createElement('DIV');
+				controlsDiv.classList += 'controls';
+				var editIcon = document.createElement('img');
+				editIcon.src = './images/edit-svg.svg';
+				var saveIcon = document.createElement('img');
+				saveIcon.src = './images/approve-svg.svg';
+				saveIcon.classList += 'hide';
+
+				var deleteIcon = document.createElement('IMG');
+				deleteIcon.src = './images/trash-svg.svg';
+				//deleteIcon.addEventListener('click', removeData(uidDiv.innerText)); //wiped entiure firebase database...
+
+				//editIcon.classList += 'fi-xnluxl-copy';
+
+				//controlsDiv.innerText = signinData['sid'];
+				//
+				listItem.appendChild(uidDiv);
+				listItem.appendChild(nameDiv);
+				listItem.appendChild(gradeDiv);
+				listItem.appendChild(sidDiv);
+				sidDiv.appendChild(copyIcon);
+				listItem.appendChild(copyIcon);
+				listItem.appendChild(problemDiv);
+				listItem.appendChild(notesDiv);
+				listItem.appendChild(timeDiv);
+				listItem.appendChild(controlsDiv);
+				controlsDiv.appendChild(editIcon);
+				controlsDiv.appendChild(saveIcon);
+				controlsDiv.appendChild(deleteIcon);
+
+				//document.getElementById('signin-list').innerHTML = childData['firstName'] + " " + childData['lastName'] + ", " + childData['grade'];
+				//document.getElementById('signin-list').appendChild(li);
+				signInList.appendChild(listItem);
+				deleteIcon.addEventListener('click', () => {
+					var confirmDelete = confirm(
+						'Are you sure you want to delete ' +
+							nameDiv.innerText +
+							' from the list?'
+					);
+					if (confirmDelete) {
+						removeData(uidDiv.innerText);
+					}
+				});
+			});
+		});
+};
+
+//Copy Button Function
+function copyFunction(e) {
+	const temp = document.createElement('textarea');
+	var copyText = e.target.parentElement.parentElement.textContent;
+	temp.value = copyText;
+	console.log(temp);
+	temp.setAttribute('readonly', '');
+	temp.style.position = 'absolute';
+	temp.style.left = '-9999px';
+	document.body.appendChild(temp);
+	temp.select();
+	document.execCommand('copy');
+	document.body.removeChild(temp);
+}
+
+document.body.addEventListener('click', e => {
+	//console.log(e.target);
+});
+
+//Copy Buttons
+var copyButtons = document.querySelectorAll('.fi-xnluxl-copy');
+for (x = 0; x < copyButtons.length; x++) {
+	copyButtons[x].addEventListener('click', e => {
+		//console.log(e.target.parentElement.parentElement.textContent);
+		copyFunction(e);
+	});
+}
+
+getData();
